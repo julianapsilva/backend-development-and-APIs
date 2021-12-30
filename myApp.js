7ee98010799230219357e4944bdbf5c4f80d1bbe
julianapsilva <julianadepaula001@gmail.com>
2021-12-30 07:50:15 -0300

    Serve Static Assets

--- myApp.js
+++ myApp.js
@@ -1,10 +1,20 @@
 var express = require('express');
 var app = express();
 console.log("Hello World")
+
+
 app.get("/", (req, res) => {
     const absolutePath = __dirname + '/views/index.html'
     res.sendFile(absolutePath)
 })
+
+app.get("/public", (req, res) => {
+    const absolutePath = __dirname + '/views/index.html'
+    res.sendFile(absolutePath)
+})
+
+app.use("/public", express.static(__dirname + "/public"));
+
 
 
 
